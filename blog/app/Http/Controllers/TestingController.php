8058cfb0d1b96e15b8647c668c5cad311634c8f4
Anri20 <88647934+Anri20@users.noreply.github.com>
2022-11-20 16:10:20 +0700

    routing laravel

---
+++ blog/app/Http/Controllers/TestingController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TestingController extends Controller
+{
+    function testing(){
+        return view('testing');
+    }
+
+    function testing2(){
+        return view('testing2');
+    }
+}
--- blog/routes/web.php
+++ blog/routes/web.php
@@ -17,6 +17,38 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+// Route::get('/namaWeb', function () {
+//     return view('namaFile');
+// });
+
+Route::view('/namaWeb', 'namaFile');
+
+Route::get('/HelloWorld', function () {
+    return "Ini adalah kalimat dan kata";
+    // return '1' + '2';
+});
+
+Route::redirect('/namaWeb', '/HelloWorld');
+
+Route::get('/param/{a}', function ($a) {
+    // return "Kamu mengetikkan " . $a;
+    return "Kamu mengetikkan $a";
+});
+
+Route::get('/param2/{a}/{b}', function ($a, $b) {
+    return $a + $b;
+    // return '1' + '2';
+});
+
+Route::get('/param3/{a?}', function ($a = 'aku adalah a') {
+    return $a;
+    // return '1' + '2';
+});
+
+// Route::get('/testing', 'TestingController@testing');
+Route::get('/testing', 'App\Http\Controllers\TestingController@testing');
+// Route::get('/testing/1', 'App\Http\Controllers\TestingController@testing2');
+
 // Route::post('/', function () {
 //     return view('welcome');
 // });
